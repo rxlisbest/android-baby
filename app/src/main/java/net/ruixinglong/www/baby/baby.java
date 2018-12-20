b94da87b9b67fca3e42c6e3b185e42d43f2faf39
Roy <rxlisbest@gmail.com>
2018-12-20 20:09:26 +0800

    weex

--- app/src/main/java/net/ruixinglong/www/baby/baby.java
+++ app/src/main/java/net/ruixinglong/www/baby/baby.java
@@ -1,52 +1,86 @@
 package net.ruixinglong.www.baby;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 
-public class baby extends AppCompatActivity {
+import com.taobao.weex.IWXRenderListener;
+import com.taobao.weex.WXSDKInstance;
+import com.taobao.weex.common.WXRenderStrategy;
+import com.taobao.weex.utils.WXFileUtils;
+
+public class baby extends AppCompatActivity implements IWXRenderListener {
+
+    WXSDKInstance mWXSDKInstance;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_baby);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        mWXSDKInstance = new WXSDKInstance(this);
+        mWXSDKInstance.registerRenderListener(this);
+        /**
+         * WXSample 可以替换成自定义的字符串，针对埋点有效。
+         * template 是.we transform 后的 js文件。
+         * option 可以为空，或者通过option传入 js需要的参数。例如bundle js的地址等。
+         * jsonInitData 可以为空。
+         * width 为-1 默认全屏，可以自己定制。
+         * height =-1 默认全屏，可以自己定制。
+         */
+        mWXSDKInstance.render("WXSample", WXFileUtils.loadFileContent("hello.js", this), null, null, -1, -1, WXRenderStrategy.APPEND_ASYNC);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_baby, menu);
-        return true;
+    public void onViewCreated(WXSDKInstance instance, View view) {
+        setContentView(view);
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    public void onRenderSuccess(WXSDKInstance instance, int width, int height) {
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    }
+
+    @Override
+    public void onRefreshSuccess(WXSDKInstance instance, int width, int height) {
+
+    }
+
+    @Override
+    public void onException(WXSDKInstance instance, String errCode, String msg) {
+
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (mWXSDKInstance != null) {
+            mWXSDKInstance.onActivityResume();
         }
+    }
 
-        return super.onOptionsItemSelected(item);
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mWXSDKInstance != null) {
+            mWXSDKInstance.onActivityPause();
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mWXSDKInstance != null) {
+            mWXSDKInstance.onActivityStop();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mWXSDKInstance != null) {
+            mWXSDKInstance.onActivityDestroy();
+        }
     }
 }
